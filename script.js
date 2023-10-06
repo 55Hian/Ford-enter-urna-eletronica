fb72dc93821995f0381dcfde9610d2d435a09265
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2023-10-06 14:59:01 +0000

    inicio do projeto

---
+++ script.js
@@ -0,0 +1,35 @@
+function inicializacao(){
+    var varvotosCandidato1 = 0;
+    var varvotosCandidato2 = 0;
+    var varvotosCandidato3 = 0;
+}
+function mostraconsole(){
+    console.log("candidato 1" + varvotosCandidato1)
+    console.log("candidato 2" + varvotosCandidato2)
+    console.log("candidato 3" + varvotosCandidato3)
+}
+
+function votocandidato1(varvotosCandidato1){
+
+    return varvotosCandidato1 +=1;
+}
+
+function votocandidato2(varvotosCandidato2){
+    return varvotosCandidato2 +=1;
+}
+
+function votocandidato3(varvotosCandidato3){
+    return varvotosCandidato3 +=1;
+}
+
+function votacao(){
+  
+
+    let numCandidato1;
+    let numCandidato2;
+    let numCandidato3;
+    let numVotoBranco;
+    let numVotoNulo;
+    let numEncerrarVotacao;
+
+}
